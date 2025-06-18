23d9535408147344ac42945af04757bcad3f2487
Omar Faruk <programmer.omar.dev@gmail.com>
2025-06-18 12:18:47 +0600

    added post hog analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { PostHogProvider } from 'posthog-js/react'
 import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={{
+        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+        capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
+        debug: import.meta.env.MODE === 'development',
+      }}
+    >
+      <App />
+    </PostHogProvider>
   </StrictMode>,
 )
